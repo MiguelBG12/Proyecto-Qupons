f02d412de78ba4e43564af0a78a33474cdf997b9
Marirosa <arqmvilcherrez@gmail.com>
2023-12-22 12:09:41 -0500

    separar procedures en diferentes carpetas

---
+++ database/procedures/usuarios/sp_verperfil_usuario.sql
@@ -0,0 +1,23 @@
+DELIMITER $$
+DROP PROCEDURE IF EXISTS `sp_verperfil_usuario` $$
+
+CREATE PROCEDURE `sp_verperfil_usuario`(
+    IN p_correo VARCHAR(20),
+    IN p_contraseña VARCHAR(64)
+
+)
+BEGIN
+	DECLARE usuario_id INT;
+    DECLARE usuario_count INT;
+
+    SELECT COUNT(*), MAX(usuario_id) INTO usuario_count, usuario_id
+    FROM `usuario`
+    WHERE `correo` = p_correo AND `contraseña` = p_contraseña;
+
+    IF usuario_count = 1 THEN
+        SELECT * FROM `usuario` WHERE `correo` = p_correo;
+    ELSE
+        SELECT 'Credenciales incorrectas' AS `mensaje_error`;
+    END IF;
+END$$
+DELIMITER ;
