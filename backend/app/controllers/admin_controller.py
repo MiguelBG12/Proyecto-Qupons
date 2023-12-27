ea57eb1f17a1801dbc7cefaad6365af604dae30e
MiguelBG12 <miguel.bg12@gmail.com>
2023-12-27 12:55:44 -0500

    se quitan los mensajes condicionales

--- backend/app/controllers/admin_controller.py
+++ backend/app/controllers/admin_controller.py
@@ -34,14 +34,6 @@ async def crear_admin(admin_request: AdminCreateRequest):
         raise HTTPException(status_code=400, detail="Ya existe un admin con este correo electronico")
     
     result = data_conexion.ejecutar_procedure('sp_crear_admin', params)
-    
-    # Verifica si hay un mensaje de error devuelto por el procedimiento almacenado
-    if 'mensaje_error' in result:
-        # Si hay un mensaje de error, se lanza una excepción con el mensaje de error
-        raise HTTPException(status_code=400, detail=f"Error al crear administrador: {result['mensaje_error']}")
-    else:
-        # Si no hay mensaje de error, se considera que la creación fue exitosa
-        return {"mensaje": "Administrador creado con éxito"}
         
 @router.post("/actualizar_admin")
 async def actualizar_admin(admin_request: AdminUpdateRequest):
@@ -54,63 +46,26 @@ async def actualizar_admin(admin_request: AdminUpdateRequest):
         hashed_password
     ]
     result = data_conexion.ejecutar_procedure('sp_actualizar_admin', params)
-    # Verifica el resultado del procedimiento almacenado
-    if 'mensaje_error' in result:
-        raise HTTPException(status_code=404, detail=f"Error al actualizar administrador: {result['mensaje_error']}")
-    else:
-        return {"mensaje": result['mensaje_exito']}
 
 @router.delete("/borrar_admin/{admin_id}")
 async def borrar_admin(admin_id: int):
     params = [admin_id]
     result = data_conexion.ejecutar_procedure('sp_borrar_admin', params)
-    
-    # Verificamos si hay un mensaje de error devuelto por el procedimiento almacenado
-    if 'MESSAGE_TEXT' in result:
-        # Si hay un mensaje de error, lanzamos una excepción con el mensaje
-        raise HTTPException(status_code=404, detail=f"Error al borrar administrador: {result['MESSAGE_TEXT']}")
-    else:
-        # Si no hay mensaje de error, consideramos que el borrado fue exitoso
-        return {"mensaje": "Administrador borrado exitosamente"}
 
 @router.delete("/borrar_cliente/{cliente_tienda_id}")
 async def borrar_cliente(cliente_tienda_id: int):
     params = [cliente_tienda_id]
     result = data_conexion.ejecutar_procedure('sp_borrar_cliente', params)
-    
-    # Verificamos si hay un mensaje de error devuelto por el procedimiento almacenado
-    if 'MESSAGE_TEXT' in result:
-        # Si hay un mensaje de error, lanzamos una excepción con el mensaje
-        raise HTTPException(status_code=404, detail=f"Error al borrar cliente: {result['MESSAGE_TEXT']}")
-    else:
-        # Si no hay mensaje de error, consideramos que el borrado fue exitoso
-        return {"mensaje": "Cliente eliminado exitosamente"}
 
 @router.delete("/borrar_cupon/{cupones_id}")
 async def borrar_cupon(cupones_id: int):
     params = [cupones_id]
     result = data_conexion.ejecutar_procedure('sp_borrar_cupon', params)
-    
-    # Verificamos si hay un mensaje de error devuelto por el procedimiento almacenado
-    if 'MESSAGE_TEXT' in result:
-        # Si hay un mensaje de error, lanzamos una excepción con el mensaje
-        raise HTTPException(status_code=404, detail=f"Error al borrar cupón: {result['MESSAGE_TEXT']}")
-    else:
-        # Si no hay mensaje de error, consideramos que el borrado fue exitoso
-        return {"mensaje": "Cupón eliminado exitosamente"}
 
 @router.delete("/borrar_usuario/{usuario_id}")
 async def borrar_usuario(usuario_id: int):
     params = [usuario_id]
     result = data_conexion.ejecutar_procedure('sp_borrar_usuario', params)
-    
-    # Verificamos si hay un mensaje de error devuelto por el procedimiento almacenado
-    if 'MESSAGE_TEXT' in result:
-        # Si hay un mensaje de error, lanzamos una excepción con el mensaje
-        raise HTTPException(status_code=404, detail=f"Error al borrar usuario: {result['MESSAGE_TEXT']}")
-    else:
-        # Si no hay mensaje de error, consideramos que el borrado fue exitoso
-        return {"mensaje": "Usuario eliminado exitosamente"}
 
 @router.get("/ver_administradores", response_model=List[dict])
 async def ver_administradores():
