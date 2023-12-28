053c69107e1c8476de5698f0bfe58306a8f427c1
MiguelBG12 <miguel.bg12@gmail.com>
2023-12-28 13:38:17 -0500

    prueba de cors con todos los permisos

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,20 @@ from app.routes import admin
 from app.routes import users
 from app.routes import stores
 from app.routes import categories
+from fastapi.middleware.cors import CORSMiddelware   
+
+origins = [
+    "http://localhost:5173"
+]
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddelware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 # Ruta del perfil del administrador
 app.include_router(admin.router, prefix="/admin", tags=["Admin"])
