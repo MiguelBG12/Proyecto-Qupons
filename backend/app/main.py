116bdbe5e7955863a96c011d0b630276b7c993d6
Denzel18rb <74129875rb@gmail.com>
2023-12-27 23:02:20 -0500

    Agregamos todo lo modificado en categorias en el archivo main !!!

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.routes import admin
 from app.routes import users
 from app.routes import stores
+from app.routes import categories
 app = FastAPI()
 
 # Agregar rutas del perfil del administrador
@@ -10,6 +11,8 @@ app.include_router(admin.router, prefix="/admin", tags=["Admin"])
 app.include_router(users.router, prefix="/user", tags=["User"])
 
 app.include_router(stores.router, prefix= "/store", tags=["Store"])
+
+app.include_router(categories.router, prefix= "/category", tags=["Category"])
 # Otros middlewares, configuraciones de base de datos, etc., pueden ir aquí
 
 if __name__ == "__main__":
