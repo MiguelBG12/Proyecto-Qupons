ec48c6250d54ce080b6e32f52daf16a345a26791
Denzel18rb <74129875rb@gmail.com>
2023-12-27 20:09:21 -0500

    Agregamos lo nuevo de store y user a nuestro archivo main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from app.routes import admin
-
+from app.routes import users
+from app.routes import stores
 app = FastAPI()
 
 # Agregar rutas del perfil del administrador
 app.include_router(admin.router, prefix="/admin", tags=["Admin"])
 
+app.include_router(users.router, prefix="/user", tags=["User"])
+
+app.include_router(stores.router, prefix= "/store", tags=["Store"])
 # Otros middlewares, configuraciones de base de datos, etc., pueden ir aquí
 
 if __name__ == "__main__":
