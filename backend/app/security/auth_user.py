9ec7e07721c12c66324025fe0803db94141798af
MiguelBG12 <miguel.bg12@gmail.com>
2023-12-30 16:28:00 -0500

    update

--- backend/app/security/auth_user.py
+++ backend/app/security/auth_user.py
@@ -3,11 +3,11 @@ import jwt
 SECRET_KEY = "your-secret-key"
 ALGORITHM = "HS256"
 
-def create_access_token(data: dict):
+def crear_token_acceso(data: dict):
     encoded_jwt = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-def decode_token(token: str):
+def decodificar_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload
