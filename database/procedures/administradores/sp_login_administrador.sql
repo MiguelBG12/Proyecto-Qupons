d08f10ac1c97d469943f06316800e67393e64134
MiguelBG12 <miguel.bg12@gmail.com>
2023-12-31 13:00:19 -0500

    fix

--- database/procedures/administradores/sp_login_administrador.sql
+++ database/procedures/administradores/sp_login_administrador.sql
@@ -1,29 +1,20 @@
 DELIMITER $$
 
-DROP PROCEDURE IF EXISTS sp_crear_admin $$
-CREATE PROCEDURE sp_crear_admin(
-    IN p_nombre VARCHAR(60),
-    IN p_cargo VARCHAR(30),
+DROP PROCEDURE IF EXISTS sp_login_administrador $$
+CREATE PROCEDURE sp_login_administrador(
     IN p_correo VARCHAR(45),
     IN p_contraseña VARCHAR(64)
 )
 BEGIN
-    DECLARE admin_count INT;
-    DECLARE hashedPassword VARCHAR(64);
+    DECLARE hashedPasswordDB VARCHAR(64);
     
-    SET hashedPassword = SHA2(p_contraseña, 256);
-
-    SELECT COUNT(*) INTO admin_count
-    FROM administradores
-    WHERE correo = p_correo;
-
-    IF admin_count = 0 THEN
-        INSERT INTO administradores (nombre, cargo, correo, contraseña)
-        VALUES (p_nombre, p_cargo, p_correo, hashedPassword);
-        SELECT 'Administrador creado con éxito' AS mensaje_exito;
-    ELSE
+    SELECT contraseña INTO hashedPasswordDB FROM administradores WHERE correo = p_correo;
+    
+    IF hashedPasswordDB IS NULL OR hashedPasswordDB <> SHA2(p_contraseña, 256) THEN
         SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Ya existe un administrador con este correo';
+        SET MESSAGE_TEXT = 'Error: Administrador o contraseña incorrectos';
+    ELSE
+        SELECT administrador_id FROM administradores WHERE correo = p_correo AND contraseña = hashedPasswordDB;
     END IF;
 END$$
 DELIMITER ;
