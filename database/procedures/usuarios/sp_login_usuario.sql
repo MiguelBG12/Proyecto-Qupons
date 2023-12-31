7281368124a47bcbc14d92b44077442e10784185
Marirosa <arqmvilcherrez@gmail.com>
2023-12-31 14:20:36 -0500

    creacion script con todos los procedures

--- database/procedures/usuarios/sp_login_usuario.sql
+++ database/procedures/usuarios/sp_login_usuario.sql
@@ -8,15 +8,12 @@ CREATE PROCEDURE `sp_login_usuario`(
 BEGIN
     DECLARE hashedPasswordDB VARCHAR(64);
     
-    -- Obtener la contraseña hasheada almacenada en la base de datos
     SELECT contraseña INTO hashedPasswordDB FROM usuario WHERE correo = p_correo;
     
-    -- Verificar si la contraseña ingresada coincide con la almacenada en la base de datos
     IF hashedPasswordDB IS NULL OR hashedPasswordDB <> SHA2(p_contraseña, 256) THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Error: Usuario o contraseña incorrectos';
     ELSE
-        -- Autenticación exitosa, devolver el usuario_id
         SELECT usuario_id FROM usuario WHERE correo = p_correo AND contraseña = hashedPasswordDB;
     END IF;
 END$$
