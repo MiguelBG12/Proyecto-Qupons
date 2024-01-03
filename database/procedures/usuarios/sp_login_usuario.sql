836c95cc027b45ec8bb83706d5717f13fca035f0
Denzel18rb <74129875rb@gmail.com>
2024-01-03 15:43:58 -0500

    corrigiendo error de fehca de naciemito en procedure login user

--- database/procedures/usuarios/sp_login_usuario.sql
+++ database/procedures/usuarios/sp_login_usuario.sql
@@ -7,17 +7,22 @@ CREATE PROCEDURE `sp_login_usuario`(
 )
 BEGIN
     DECLARE hashedPasswordDB VARCHAR(256);
+    DECLARE hashedFechaNacimientoDB VARCHAR(64);
     
-    SELECT contrasenna INTO hashedPasswordDB FROM usuario WHERE correo = p_correo;
+    SELECT contrasenna, SHA2(fecha_nacimiento, 256) INTO hashedPasswordDB, hashedFechaNacimientoDB
+    FROM usuario WHERE correo = p_correo;
+    
     IF hashedPasswordDB IS NULL THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Error: Usuario o contraseña incorrectos';
     ELSE
         IF hashedPasswordDB = SHA2(p_contrasenna, 256) THEN
-            SELECT `usuario_id`,`nombres_completos`, `dni`, `genero`, `fecha_nacimiento`, `direccion`, `departamento`, `correo`, `contrasenna`, `telefono`, 3 AS `rol_id` FROM usuario WHERE correo = p_correo;
+            SELECT `usuario_id`, `nombres_completos`, `dni`, `genero`, hashedFechaNacimientoDB AS `fecha_nacimiento_hashed`, `direccion`, `departamento`, `correo`, `contrasenna`, `telefono`, 3 AS `rol_id` 
+            FROM usuario 
+            WHERE correo = p_correo;
         ELSE
             SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = 'Error: Tienda o contraseña incorrectos';
+            SET MESSAGE_TEXT = 'Error: Usuario o contraseña incorrectos';
         END IF;
     END IF;
 END$$
