a9b03b6ffde95df9a3ad4babd438d29a9e84cf09
Denzel18rb <74129875rb@gmail.com>
2024-01-03 16:57:31 -0500

    correcion de procedure login usuario "credenciales incorrectas"

--- database/procedures/usuarios/sp_login_usuario.sql
+++ database/procedures/usuarios/sp_login_usuario.sql
@@ -1,4 +1,5 @@
 DELIMITER $$
+
 DROP PROCEDURE IF EXISTS `sp_login_usuario` $$
 CREATE PROCEDURE `sp_login_usuario`(
     IN p_correo VARCHAR(45),
@@ -6,18 +7,22 @@ CREATE PROCEDURE `sp_login_usuario`(
 )
 BEGIN
     DECLARE hashedPasswordDB VARCHAR(256);
-
-    SELECT contraseña INTO hashedPasswordDB FROM usuario WHERE correo = p_correo;
-    SELECT contrasenna INTO hashedPasswordDB FROM usuario WHERE correo = p_correo;
+    DECLARE fechaNacimientoDB DATE;
+    
+    SELECT contrasenna, fecha_nacimiento INTO hashedPasswordDB, fechaNacimientoDB
+    FROM usuario WHERE correo = p_correo;
+    
     IF hashedPasswordDB IS NULL THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Error: Usuario o contraseña incorrectos';
     ELSE
         IF hashedPasswordDB = SHA2(p_contrasenna, 256) THEN
-            SELECT `usuario_id`,`nombres_completos`, `dni`, `genero`, `fecha_nacimiento`, `direccion`, `departamento`, `correo`, `contrasenna`, `telefono`, 3 AS `rol_id` FROM usuario WHERE correo = p_correo;
+            SELECT `usuario_id`, `nombres_completos`, `dni`, `genero`, fechaNacimientoDB AS `fecha_nacimiento`, `direccion`, `departamento`, `correo`, `contrasenna`, `telefono`, 3 AS `rol_id` 
+            FROM usuario 
+            WHERE correo = p_correo;
         ELSE
             SIGNAL SQLSTATE '45000'
-            SET MESSAGE_TEXT = 'Error: Tienda o contraseña incorrectos';
+            SET MESSAGE_TEXT = 'Error: Usuario o contraseña incorrectos';
         END IF;
     END IF;
 END$$
