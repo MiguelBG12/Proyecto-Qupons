ae0aca407c077f7dfa37f31c4ee1a196674f1ba9
Denzel18rb <74129875rb@gmail.com>
2024-01-04 20:32:16 -0500

    corrigiendo login de usuario en backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,13 @@
-from fastapi import FastAPI, Depends, HTTPException, Response
-from fastapi import Request
-from app.routes.admin import router as admin_router
+from jose import jwt
+from fastapi import FastAPI, Depends, HTTPException, Request, Response
 from app.routes import admin
 from app.routes import users
 from app.routes import stores
 from app.routes import registro
-from app.models.administrator import AdminCreateRequest, AdminLoginRequest
-from app.config.db_conexion import data_conexion
+from app.models.administrator import AdminLoginRequest
 from app.utils.utils import create_access_token, get_current_user, SECRET_KEY, ALGORITHM
+from app.config.db_conexion import data_conexion
 from fastapi.middleware.cors import CORSMiddleware
-import jwt  # Asegúrate de importar el módulo jwt
 from fastapi.responses import JSONResponse
 
 # Configuración de orígenes permitidos para CORS
@@ -28,6 +26,7 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+# Endpoint para autenticar y generar tokens de acceso
 @app.post("/login")
 async def login_admin(admin_request: AdminLoginRequest, response: Response):
     params = [
@@ -35,13 +34,31 @@ async def login_admin(admin_request: AdminLoginRequest, response: Response):
         admin_request.contrasenna
     ]
     admins = data_conexion.ejecutar_procedure('sp_login_administrador', params)
+
+    stores = data_conexion.ejecutar_procedure('sp_login_tienda', params)
+
+    users = data_conexion.ejecutar_procedure('sp_login_usuario', params)
+
     user = None
     if admins is not None and 'result' in admins and admins['result']:
         user = admins['result'][0][0]
+        
+    if stores is not None and 'result' in stores and stores['result']:
+        user = stores['result'][0][0]
 
+    if users is not None and 'result' in users and users['result']:
+        user = users['result'][0][0]
+        
     if user is not None:
-        access_token = create_access_token(data=user)
+        if user is not None:
+        # Convertir la fecha de nacimiento para que se pueda visualisar de manera correcta
+            fecha_nacimiento = user.get("fecha_nacimiento")
+        if fecha_nacimiento:
+            user["fecha_nacimiento"] = fecha_nacimiento.isoformat()
 
+        # Generar token JWT si las credenciales son válidas
+        access_token = create_access_token(data=user) #access_token = create_access_token(data={"sub": admin_request.correo})
+        
         # Establecer la cookie con el token
         response.set_cookie(
             key="access_token",
@@ -49,9 +66,9 @@ async def login_admin(admin_request: AdminLoginRequest, response: Response):
             httponly=True,
             max_age=1800  # max_age en segundos (ej. 1800 = 30 minutos)
         )
-
+        
         return {"access_token": access_token, "token_type": "bearer"}
-
+    
     # La respuesta CORS debe ir después de devolver el token
     response.headers["Access-Control-Allow-Origin"] = "http://localhost:5173"
     raise HTTPException(status_code=401, detail="Credenciales incorrectas")
@@ -66,21 +83,25 @@ async def admin_token_validation(request: Request, call_next):
     if request.url.path.startswith("/login") == False:
         token = request.headers.get("Authorization", "").replace("Bearer ", "").strip()
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        # username: str = payload.get("username")
+        #username: str = payload.get("username")
         role_id: int = payload.get("rol_id")
 
         if request.url.path.startswith("/admin"):
             verified = (role_id == 1)
+            
         elif request.url.path.startswith("/stores"):
             verified = (role_id == 2)
+
         elif request.url.path.startswith("/users"):
             verified = (role_id == 3)
+        
         else:
             verified = False
 
         if verified:
             return await call_next(request)
         else:
+            #print("Token no proporcionado")
             raise HTTPException(status_code=403, detail="No tienes permiso para acceder")
 
     return await call_next(request)
@@ -88,8 +109,8 @@ async def admin_token_validation(request: Request, call_next):
 # Rutas
 app.include_router(registro.router, prefix="/registro", tags=["Registro"])
 app.include_router(admin.router, prefix="/admin", tags=["Admin"])
-app.include_router(users.router, prefix="/user", tags=["User"])
-app.include_router(stores.router, prefix="/store", tags=["Store"])
+app.include_router(users.router, prefix="/users", tags=["User"])
+app.include_router(stores.router, prefix="/stores", tags=["Store"])
 
 # Ejecución de la aplicación
 if __name__ == "__main__":
--- database/procedures/usuarios/sp_login_usuario.sql
+++ database/procedures/usuarios/sp_login_usuario.sql
@@ -1,5 +1,4 @@
 DELIMITER $$
-
 DROP PROCEDURE IF EXISTS `sp_login_usuario` $$
 CREATE PROCEDURE `sp_login_usuario`(
     IN p_correo VARCHAR(45),
@@ -7,19 +6,15 @@ CREATE PROCEDURE `sp_login_usuario`(
 )
 BEGIN
     DECLARE hashedPasswordDB VARCHAR(256);
-    DECLARE fechaNacimientoDB DATE;
-    
-    SELECT contrasenna, fecha_nacimiento INTO hashedPasswordDB, fechaNacimientoDB
-    FROM usuario WHERE correo = p_correo;
-    
+
+    SELECT contraseña INTO hashedPasswordDB FROM usuario WHERE correo = p_correo;
+    SELECT contrasenna INTO hashedPasswordDB FROM usuario WHERE correo = p_correo;
     IF hashedPasswordDB IS NULL THEN
         SIGNAL SQLSTATE '45000'
         SET MESSAGE_TEXT = 'Error: Usuario o contraseña incorrectos';
     ELSE
         IF hashedPasswordDB = SHA2(p_contrasenna, 256) THEN
-            SELECT `usuario_id`, `nombres_completos`, `dni`, `genero`, fechaNacimientoDB AS `fecha_nacimiento`, `direccion`, `departamento`, `correo`, `contrasenna`, `telefono`, 3 AS `rol_id` 
-            FROM usuario 
-            WHERE correo = p_correo;
+            SELECT `usuario_id`,`nombres_completos`, `dni`, `genero`, `fecha_nacimiento`, `direccion`, `departamento`, `correo`, `contrasenna`, `telefono`, 3 AS `rol_id` FROM usuario WHERE correo = p_correo;
         ELSE
             SIGNAL SQLSTATE '45000'
             SET MESSAGE_TEXT = 'Error: Usuario o contraseña incorrectos';
